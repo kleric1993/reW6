28e51842153859bf6299a0fa0e0f1b2b4b622c46
Eric <kloukoueric@gmail.com>
2024-05-08 16:08:27 -0700

    new modif

--- output.tf
+++ output.tf
@@ -1,3 +1,6 @@
 output "public_ip" {
-  value = aws_lightsail_instance.my_server.public_ip.id
+  value = aws_lightsail_instance.server1.public_ip_address
+}
+output "private_ip" {
+    value = aws_lightsail_instance.server1.private_ip_address
 }
