fd1c00301cd02e280b1ee7e54221ef77eeac21d8
Eric <kloukoueric@gmail.com>
2024-05-08 15:13:17 -0700

    lightsail

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
-resource "aws_lightsail_instance" "custom" {
-  name              = "my_server"
-  availability_zone = "us-east-1b"
-  blueprint_id      = "amazon_linux_2"
-  bundle_id         = "nano_3_0"
-  user_data         = "sudo yum install -y httpd && sudo systemctl start httpd && sudo systemctl enable httpd && echo '<h1>Deployed via Terraform</h1>' | sudo tee /var/www/html/index.html"
+resource "aws_lightsail_instance" "server1" {
+  name = "dev-server"
+  blueprint_id = "ubuntu_18_04"
+  bundle_id = "medium_1_0"
+  availability_zone = "us-east-1a"
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update
+              sudo apt-get install -y apache2
+              sudo systemctl start apache2
+              sudo systemctl enable apache2
+              echo '<h1>This is deployed by Serge </h1>' | sudo tee /var/www/html/index.html
+              sudo useradd serge 
+              EOF
 }
